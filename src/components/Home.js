1a10b40afc66acaee71ebda1ede0a48c0990917f
louisroman <louisroman133@gmail.com>
2019-04-26 06:28:22 -0400

    updates

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,12 @@
 import React from 'react'
-
+import {Link} from 'react-router-dom'
 
 class Home extends React.Component {
     render(){
     return(
        <div className='homeContainer'>
            <h2>Welcome!</h2>
-           <h3>Please <a href='/login'>Login</a> or <a href ='/signup'>Sign up</a>!</h3>
+           <h3>Please <Link to='/login'>Login</Link> or <Link to='/signin'>Sign In </Link>!</h3>
        </div>
     )
     }
