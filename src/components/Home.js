dd867144c3de0ec56cadbf75b5ddc9e3e5d632b5
louisroman <louisroman133@gmail.com>
2019-04-26 06:24:48 -0400

    updates

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,7 +5,8 @@ class Home extends React.Component {
     render(){
     return(
        <div className='homeContainer'>
-           <h2>Welcome! Home</h2>
+           <h2>Welcome!</h2>
+           <h3>Please <a href='/login'>Login</a> or <a href ='/signup'>Sign up</a>!</h3>
        </div>
     )
     }
