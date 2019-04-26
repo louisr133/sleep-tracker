4fd41c8b4ccb16871fcf5ad05257c004b695156b
louisroman <louisroman133@gmail.com>
2019-04-26 06:16:07 -0400

    updates

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,13 +2,9 @@ import React from 'react'
 
 
 class Home extends React.Component {
-    render(){
-    return(
-       <div className='homeContainer'>
-           <h2>Welcome! Home</h2>
-       </div>
-    )
-    }
+  componentDidMount(){
+    this.props.history.push('/login')
+  }
 }
 
 export default Home
